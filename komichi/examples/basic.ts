8a2632e6e2cfa5399e2f0504294bdcd053707d30
Shunnsuke-cloud <u.shunke@gmail.com>
2026-07-20 17:55:53 +0900

    土台制作

---
+++ komichi/examples/basic.ts
@@ -0,0 +1,5 @@
+import { Komichi } from "../src/index.js";
+
+const app = new Komichi();
+
+app.listen(3000);
---
+++ komichi/src/index.ts
@@ -0,0 +1,5 @@
+export class Komichi {
+  listen(port: number): void {
+    console.log(`Komichi is running on http://localhost:${port}`);
+  }
+}
